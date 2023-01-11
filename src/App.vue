a5cf0f1535e516ae56fa55b3b3ca364974c234bb
牧海 <292345300@qq.com>
2023-01-12 02:30:10 +0800

    build(custom): 测试

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>1232323测试</div>
+  <div>1232323测试dsadsad</div>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue'
   export default defineComponent({
     name: 'App',
     setup() {
-    },
+    }
   })
 </script>
 <style></style>
