79b46d8a254f98a0b33b229ea4900f92b83c8f31
牧海 <292345300@qq.com>
2023-01-12 09:42:04 +0800

    fix(custom): 遗漏改动提交

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>1232323测试dsadsad</div>
+    <div>1232323测试dsadsad</div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue'
-  export default defineComponent({
-    name: 'App',
-      setup() {
-        
-    }
-  })
+    import { defineComponent } from 'vue'
+    export default defineComponent({
+        name: 'App',
+        setup() {
+            const name = ''
+        }
+    })
 </script>
 <style></style>
